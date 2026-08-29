ab0d5fea92d710f6d885835822dae1aa0db38951
Thomas <thomashii@dukefirehawk.com>
2026-08-29 12:02:54 +0800

    refactor: simplify project initialization and remove redundant boilerplate cloning logic

--- lib/src/commands/init.dart
+++ lib/src/commands/init.dart
@@ -1,16 +1,15 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:archive/archive_io.dart';
 import 'package:args/command_runner.dart';
 import 'package:dcli/dcli.dart' as dcli;
-import 'package:http/http.dart' as http;
 import 'package:io/ansi.dart';
 import 'package:path/path.dart' as p;
-import 'package:prompts/prompts.dart' as prompts;
+import 'package:recase/recase.dart';
 
+import '../random_string.dart' as rs;
 import '../util.dart';
-import 'key.dart';
+import 'commands.dart';
 
 class InitCommand extends Command {
   final KeyCommand _key = KeyCommand();
@@ -51,8 +50,7 @@ class InitCommand extends Command {
     print('Creating new Angel3 project in ${projectDir.absolute.path}...');
     await _cloneRepo(projectDir);
 
-    /*
-    // await preBuild(projectDir);
+    await preBuild(projectDir);
     var secret = rs.randomAlphaNumeric(32);
     print('Generated new development JWT secret: $secret');
     await _key.changeSecret(
@@ -85,7 +83,7 @@ class InitCommand extends Command {
     }
 
     print(green.wrap('$checkmark Successfully initialized Angel3 project.'));
- */
+
     stdout
       ..writeln()
       ..writeln(
@@ -100,10 +98,6 @@ class InitCommand extends Command {
       ..writeln('Find more documentation about Angel3:')
       ..writeln('  * https://angel3-framework.web.app')
       ..writeln('  * https://angel3-docs.dukefirehawk.com')
-      //      ..writeln(
-      //          '  * https://www.youtube.com/playlist?list=PLl3P3tmiT-frEV50VdH_cIrA2YqIyHkkY')
-      //      ..writeln('  * https://medium.com/the-angel-framework')
-      //      ..writeln('  * https://dart.academy/tag/angel')
       ..writeln()
       ..writeln('Happy coding!');
   }
@@ -145,7 +139,7 @@ class InitCommand extends Command {
   }
 
   Future _cloneRepo(Directory projectDir) async {
-    //Directory boilerplateDir = Directory("./empty");
+    Directory boilerplateDir = Directory("./empty");
     try {
       if (await projectDir.exists()) {
         bool shouldDelete = dcli.confirm(
@@ -168,27 +162,21 @@ class InitCommand extends Command {
         defaultOption: basicBoilerplate,
       );
 
-      // Ultimately, we want a clone of every boilerplate locally on the system.
+      // Save the templates locally on the system.
       var boilerplateRootDir = Directory(p.join(angelDir.path, 'boilerplates'));
-      //var boilerplateBasename = p.basenameWithoutExtension(boilerplate.url);
-      //if (boilerplate.ref != '') {
-      //  boilerplateBasename += '.${boilerplate.ref}';
-      //}
-
       await boilerplateRootDir.create(recursive: true);
-
-      var boilerplateDir = Directory(
+      boilerplateDir = Directory(
         p.join(boilerplateRootDir.path, boilerplate.subfolderPath),
       );
       if (!await boilerplateDir.exists()) {
         if (argResults!['offline'] as bool) {
           throw Exception(
-            '--offline was selected, but the "${boilerplate.name}" boilerplate has not yet been downloaded.',
+            '--offline was selected, but the "${boilerplate.name}" template has not yet been downloaded.',
           );
         }
 
         print(
-          'Downloading "${boilerplate.name}" boilerplate from "${boilerplate.url}"...',
+          'Downloading "${boilerplate.name}" template from "${boilerplate.url}"...',
         );
 
         await downloadAndExtractSubfolder(
@@ -198,13 +186,20 @@ class InitCommand extends Command {
         );
       } else {
         print(
-          'Using cached "${boilerplate.name}" boilerplate from "${boilerplateRootDir.path}".',
+          'Using cached "${boilerplate.name}" template from "${boilerplateRootDir.path}".',
         );
       }
+
+      // Copy the required template into the given directory.
+      await copyDirectory(boilerplateDir, projectDir);
+
+      if (boilerplate.needsPrebuild) {
+        await preBuild(projectDir).catchError((_) => null);
+      }
     } catch (e) {
-      //if (await boilerplateDir.exists()) {
-      //  await boilerplateDir.delete(recursive: true);
-      //}
+      if (await boilerplateDir.exists()) {
+        await boilerplateDir.delete(recursive: true);
+      }
 
       if (e is! String) {
         print(dcli.red('$ballot Could not initialize Angel3 project.'));
@@ -213,201 +208,7 @@ class InitCommand extends Command {
     }
   }
 
-  Future _cloneRepoOri(Directory projectDir) async {
-    Directory boilerplateDir = Directory("./empty");
-
-    try {
-      if (await projectDir.exists()) {
-        var shouldDelete = prompts.getBool(
-          "Directory '${projectDir.absolute.path}' already exists. Overwrite it?",
-        );
-
-        if (!shouldDelete) {
-          throw 'Chose not to overwrite existing directory.';
-        } else if (projectDir.absolute.uri.normalizePath().toFilePath() !=
-            Directory.current.absolute.uri.normalizePath().toFilePath()) {
-          await projectDir.delete(recursive: true);
-        } else {
-          await _deleteRecursive(projectDir, false);
-        }
-      }
-
-      // var boilerplate = basicBoilerplate;
-      print('Choose a project type before continuing:');
-
-      var boilerplate =
-          prompts.choose(
-            'Choose a project type before continuing',
-            boilerplates,
-          ) ??
-          basicBoilerplate;
-
-      // Ultimately, we want a clone of every boilerplate locally on the system.
-      var boilerplateRootDir = Directory(p.join(angelDir.path, 'boilerplates'));
-      var boilerplateBasename = p.basenameWithoutExtension(boilerplate.url);
-      if (boilerplate.ref != '') {
-        boilerplateBasename += '.${boilerplate.ref}';
-      }
-      boilerplateDir = Directory(
-        p.join(boilerplateRootDir.path, boilerplateBasename),
-      );
-      await boilerplateRootDir.create(recursive: true);
-
-      var branch = boilerplate.ref;
-      if (branch == '') {
-        branch = 'master';
-      }
-
-      // If there is no clone existing, clone it.
-      if (!await boilerplateDir.exists()) {
-        if (argResults!['offline'] as bool) {
-          throw Exception(
-            '--offline was selected, but the "${boilerplate.name}" boilerplate has not yet been downloaded.',
-          );
-        }
-
-        print(
-          'Cloning "${boilerplate.name}" boilerplate from "${boilerplate.url}"...',
-        );
-        Process git;
-
-        if (boilerplate.ref == '') {
-          print(
-            darkGray.wrap(
-              '\$ git clone --depth 1 ${boilerplate.url} ${boilerplateDir.absolute.path}',
-            ),
-          );
-          git = await Process.start('git', [
-            'clone',
-            '--depth',
-            '1',
-            boilerplate.url,
-            boilerplateDir.absolute.path,
-          ], mode: ProcessStartMode.inheritStdio);
-        } else {
-          // git clone --single-branch -b branch host:/dir.git
-          print(
-            darkGray.wrap(
-              '\$ git clone --depth 1 --single-branch -b ${boilerplate.ref} ${boilerplate.url} ${boilerplateDir.absolute.path}',
-            ),
-          );
-          git = await Process.start('git', [
-            'clone',
-            '--depth',
-            '1',
-            '--single-branch',
-            '-b',
-            boilerplate.ref,
-            boilerplate.url,
-            boilerplateDir.absolute.path,
-          ], mode: ProcessStartMode.inheritStdio);
-        }
-
-        if (await git.exitCode != 0) {
-          throw Exception('Could not clone repo.');
-        }
-      }
-      // Otherwise, pull from git.
-      else if (!(argResults!['offline'] as bool)) {
-        print(darkGray.wrap('\$ git pull origin $branch'));
-        var git = await Process.start(
-          'git',
-          ['pull', 'origin', branch],
-          mode: ProcessStartMode.inheritStdio,
-          workingDirectory: boilerplateDir.absolute.path,
-        );
-        if (await git.exitCode != 0) {
-          print(
-            yellow.wrap(
-              'Update of $branch failed. Attempting to continue with existing contents.',
-            ),
-          );
-        }
-      } else {
-        print(
-          darkGray.wrap(
-            'Using existing contents of "${boilerplate.name}" boilerplate.',
-          ),
-        );
-      }
-
-      // Next, just copy everything into the given directory.
-      await copyDirectory(boilerplateDir, projectDir);
-
-      if (boilerplate.needsPrebuild) {
-        await preBuild(projectDir).catchError((_) => null);
-      }
-
-      var gitDir = Directory.fromUri(projectDir.uri.resolve('.git'));
-      if (await gitDir.exists()) await gitDir.delete(recursive: true);
-    } catch (e) {
-      await boilerplateDir.delete(recursive: true);
-      //await boilerplateDir.delete(recursive: true).catchError((e) {
-      //  print('Got error: ${e.error}');
-      //});
-
-      if (e is! String) {
-        print(red.wrap('$ballot Could not initialize Angel3 project.'));
-      }
-      rethrow;
-    }
-  }
-
-  /// Downloads a specific subfolder from a GitHub repository and extracts it locally.
-  ///
-  /// [repoUrl] - Repository Url
-  /// [outputDir] - Local directory where files should be extracted
-  /// [ref] - Branch, tag, or commit hash (defaults to 'main')
-  Future<void> downloadAndExtractSubfolder({
-    required String repoUrl,
-    required Directory outputDir,
-    String ref = 'main',
-  }) async {
-    // 1. Construct GitHub zipball download URL
-    final zipUrl = Uri.parse('$repoUrl/$ref');
-
-    print('Downloading repository archive...');
-    final response = await http.get(
-      zipUrl,
-      headers: {'Accept': 'application/vnd.github.v3+json'},
-    );
-
-    if (response.statusCode != 200) {
-      throw Exception(
-        'Failed to download archive: ${response.statusCode} - ${response.reasonPhrase}',
-      );
-    }
-
-    // Decode the ZIP archive in memory
-    final archive = ZipDecoder().decodeBytes(response.bodyBytes);
-    //final extractDir = Directory(outputDir.path);
-
-    // Extract files individually
-    print('Extracting files...');
-    for (final file in archive) {
-      // Split relative path segments
-      final parts = file.name.split('/');
-
-      // Skip root folder segment
-      if (parts.length > 1) {
-        final relativePath = parts.sublist(1).join('/');
-        if (relativePath.isEmpty) continue;
-
-        final fullPath = '${outputDir.path}/$relativePath';
-
-        if (file.isFile) {
-          final outFile = File(fullPath);
-          outFile.createSync(recursive: true);
-          outFile.writeAsBytesSync(file.content as List<int>);
-        } else {
-          Directory(fullPath).createSync(recursive: true);
-        }
-      }
-    }
-    print('Extraction complete!');
-  }
-
-  Future _pubGet(Directory projectDir) async {
+  Future<void> _pubGet(Directory projectDir) async {
     var dartPath = "dart";
     print(darkGray.wrap('Running "$dartPath"...'));
     print(darkGray.wrap('\$ $dartPath pub get'));
@@ -422,7 +223,7 @@ class InitCommand extends Command {
   }
 }
 
-Future preBuild(Directory projectDir) async {
+Future<void> preBuild(Directory projectDir) async {
   // Run build
   // print('Running `dart run build_runner build`...');
   print(darkGray.wrap('\$ dart run build_runner build'));
--- lib/src/util.dart
+++ lib/src/util.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:archive/archive_io.dart';
+import 'package:http/http.dart' as http;
 import 'package:io/ansi.dart';
 import 'package:path/path.dart' as p;
 import 'package:pubspec_parse/pubspec_parse.dart';
@@ -26,7 +28,6 @@ Future<Pubspec> loadPubspec([Directory? directory]) {
   );
 }
 
-// From: https://gist.github.com/tobischw/98dcd2563eec9a2a87bda8299055358a
 Future<void> copyDirectory(Directory source, Directory destination) async {
   // if (!topLevel) stdout.write('\r');
   // print(darkGray
@@ -77,4 +78,58 @@ Future<bool> runCommand(String exec, List<String> args) async {
     print(red.wrap('$ballot Failed to run process.'));
     return false;
   }
+}
+
+/// Downloads a specific subfolder from a GitHub repository and extracts it locally.
+///
+/// [repoUrl] - Repository Url
+/// [outputDir] - Local directory where files should be extracted
+/// [ref] - Branch, tag, or commit hash (defaults to 'main')
+Future<void> downloadAndExtractSubfolder({
+  required String repoUrl,
+  required Directory outputDir,
+  String ref = 'main',
+}) async {
+  // 1. Construct GitHub zipball download URL
+  final zipUrl = Uri.parse('$repoUrl/$ref');
+
+  print('Downloading repository archive...');
+  final response = await http.get(
+    zipUrl,
+    headers: {'Accept': 'application/vnd.github.v3+json'},
+  );
+
+  if (response.statusCode != 200) {
+    throw Exception(
+      'Failed to download archive: ${response.statusCode} - ${response.reasonPhrase}',
+    );
+  }
+
+  // Decode the ZIP archive in memory
+  final archive = ZipDecoder().decodeBytes(response.bodyBytes);
+  //final extractDir = Directory(outputDir.path);
+
+  // Extract files individually
+  print('Extracting files...');
+  for (final file in archive) {
+    // Split relative path segments
+    final parts = file.name.split('/');
+
+    // Skip root folder segment
+    if (parts.length > 1) {
+      final relativePath = parts.sublist(1).join('/');
+      if (relativePath.isEmpty) continue;
+
+      final fullPath = '${outputDir.path}/$relativePath';
+
+      if (file.isFile) {
+        final outFile = File(fullPath);
+        outFile.createSync(recursive: true);
+        outFile.writeAsBytesSync(file.content as List<int>);
+      } else {
+        Directory(fullPath).createSync(recursive: true);
+      }
+    }
+  }
+  print('Extraction complete!');
 }
